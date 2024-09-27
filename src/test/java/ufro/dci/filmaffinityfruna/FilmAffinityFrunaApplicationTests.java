1ef735155573fa8c156e2f902e67e6a633cd6bd8
Andres <andresalvarez360@gmail.com>
2024-09-27 03:26:58 -0300

    Agregando estructura basica de spring boot, configuracion para thymeleaf e index

---
+++ src/test/java/ufro/dci/filmaffinityfruna/FilmAffinityFrunaApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.dci.filmaffinityfruna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmAffinityFrunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
