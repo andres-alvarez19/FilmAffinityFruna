da82f5bb1b372bf00b3f2157fd742a77a78e4139
j1gnacio <135149069+j1gnacio@users.noreply.github.com>
2024-11-09 18:53:46 -0300

    Update DirectorService.java

--- src/main/java/ufro/dci/filmaffinityfruna/service/DirectorService.java
+++ src/main/java/ufro/dci/filmaffinityfruna/service/DirectorService.java
@@ -1,65 +1,39 @@
 package ufro.dci.filmaffinityfruna.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ufro.dci.filmaffinityfruna.model.entity.DirectorEntity;
 import ufro.dci.filmaffinityfruna.repository.DirectorRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
-public class DirectorService {
+public class DirectorService extends BaseService<DirectorEntity, Long> {
 
     private final DirectorRepository directorRepository;
-    private final ValidationService validationService; // Inyectamos el ValidationService
 
+    public DirectorService(DirectorRepository directorRepository) {
+        super(directorRepository);
+        this.directorRepository = directorRepository;
+    }
+
+    @Override
+    public boolean existsByUniqueProperty(DirectorEntity directorEntity) {
+        return directorRepository.existsByName(directorEntity.getName());
+    }
+
+    @Override
     public void register(DirectorEntity directorEntity) {
-        // Delegamos la validación al ValidationService
-        if (validationService.existsDirector(directorEntity.getName())) {
-            throw new IllegalArgumentException("El nombre del director ya está registrado");
-        } else {
-            directorRepository.save(directorEntity);
+        if (existsByUniqueProperty(directorEntity)) {
+            throw new IllegalArgumentException("El director ya está registrado");
         }
-    }
-
-    public void update(Long id, DirectorEntity modifiedDirector) {
-        Optional<DirectorEntity> optionalDirector = directorRepository.findById(id);
-
-        // Validamos si el nombre del director ya está registrado
-        if (validationService.existsDirector(modifiedDirector.getName())) {
-            throw new IllegalArgumentException("El nombre del director ya está registrado");
-        } else {
-            if (optionalDirector.isPresent()) {
-                DirectorEntity director = optionalDirector.get();
-                director.setName(modifiedDirector.getName());
-                director.setNationality(modifiedDirector.getNationality());
-                director.setDateOfBirth(modifiedDirector.getDateOfBirth());
-                director.setDateOfDeath(modifiedDirector.getDateOfDeath());
-                director.setWikipediaLink(modifiedDirector.getWikipediaLink());
-                directorRepository.save(director);
-            } else {
-                throw new IllegalArgumentException("Director no encontrado");
-            }
-        }
-    }
-
-    public void deleteDirectorById(Long id) {
-        // Validamos si el director existe antes de eliminarlo
-        if (!directorRepository.existsById(id)) {
-            throw new IllegalArgumentException("Director no encontrado");
-        } else {
-            directorRepository.deleteById(id);
-        }
+        super.register(directorEntity);
     }
 
     public List<DirectorEntity> searchByName(String name) {
-        // Validamos si el director existe antes de devolver la lista
-        if (!directorRepository.existsByName(name)) {
+        List<DirectorEntity> directors = directorRepository.findByName(name);
+        if (directors.isEmpty()) {
             throw new IllegalArgumentException("Director no encontrado");
-        } else {
-            return directorRepository.findByName(name);
         }
+        return directors;
     }
 }
