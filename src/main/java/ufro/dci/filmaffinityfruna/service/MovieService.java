6420d50673892f74847cbf73ea6561895d07ca1f
j1gnacio <135149069+j1gnacio@users.noreply.github.com>
2024-11-09 06:50:13 -0300

    Update MovieService.java

--- src/main/java/ufro/dci/filmaffinityfruna/service/MovieService.java
+++ src/main/java/ufro/dci/filmaffinityfruna/service/MovieService.java
@@ -1,55 +1,49 @@
 package ufro.dci.filmaffinityfruna.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ufro.dci.filmaffinityfruna.model.entity.MovieEntity;
 import ufro.dci.filmaffinityfruna.repository.MovieRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
-public class MovieService {
+public class MovieService extends BaseService<MovieEntity, Long> {
 
     private final MovieRepository movieRepository;
 
+    public MovieService(MovieRepository movieRepository) {
+        super(movieRepository);
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
     public void register(MovieEntity movieEntity) {
         if (movieRepository.existsByName(movieEntity.getName())) {
             throw new IllegalArgumentException("La película ya está registrada");
-        } else {
-            movieRepository.save(movieEntity);
         }
+        super.register(movieEntity);
     }
 
-    public void update(long id, MovieEntity updatedMovie) {
-        Optional<MovieEntity> existingMovie = movieRepository.findById(id);
-        if (existingMovie.isPresent()) {
-            MovieEntity movie = existingMovie.get();
-            movie.setName(updatedMovie.getName());
-            movie.setReleaseYear(updatedMovie.getReleaseYear());
-            movie.setDuration(updatedMovie.getDuration());
-            movie.setGenre(updatedMovie.getGenre());
-            movie.setCast(updatedMovie.getCast());
-            movieRepository.save(movie);
-        } else {
-            throw new IllegalArgumentException("Película no encontrada");
-        }
-    }
-
-    public void deleteMovieById(long id) {
+    @Override
+    public void update(Long id, MovieEntity updatedMovie) {
         if (!movieRepository.existsById(id)) {
             throw new IllegalArgumentException("Película no encontrada");
-        } else {
-            movieRepository.deleteById(id);
         }
+        super.update(id, updatedMovie);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (!movieRepository.existsById(id)) {
+            throw new IllegalArgumentException("Película no encontrada");
+        }
+        super.deleteById(id);
     }
 
     public List<MovieEntity> searchByName(String name) {
         if (!movieRepository.existsByName(name)) {
             throw new IllegalArgumentException("Película no encontrada");
-        } else {
-            return movieRepository.findByName(name);
         }
+        return movieRepository.findByName(name);
     }
 }
