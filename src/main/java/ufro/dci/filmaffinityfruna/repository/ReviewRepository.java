32b63251aade9aa2fc05ea349578d1752613b6a4
Andres <andresalvarez360@gmail.com>
2024-12-16 00:01:49 -0300

    Limpiando metodos sin usar

--- src/main/java/ufro/dci/filmaffinityfruna/repository/ReviewRepository.java
+++ src/main/java/ufro/dci/filmaffinityfruna/repository/ReviewRepository.java
@@ -4,12 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ufro.dci.filmaffinityfruna.model.entity.ReviewEntity;
 
-import java.util.List;
-
 @Repository
 public interface ReviewRepository extends CrudRepository<ReviewEntity, Long> {
-    List<ReviewEntity> findByMovieId(Long movieId);
-
     ReviewEntity findReviewById(Long id);
 
     ReviewEntity findReviewsByUserId(long userId);
